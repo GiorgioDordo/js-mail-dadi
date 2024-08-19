974858a2c3bb6187dd02fede28d58975410b6923
GiorgioDordo <djole6789@gmail.com>
2024-08-19 17:58:42 +0200

    exercise completed

--- dadi/script.js
+++ dadi/script.js
@@ -1,8 +1,21 @@
 console.log('ok');
 
 const userNumber = document.getElementById('user');
-userNumber.innerHTML = `<strong>PC:<strong> ${Math.floor(Math.random() * 11)}`;
+userValue = Math.floor(Math.random() * 11);
+console.log(userValue);
+userNumber.innerHTML = `<strong>PC:<strong> ${userValue}`;
 
 const pcNumber = document.getElementById('pc');
-pcNumber.innerHTML = `<strong>PC:<strong> ${Math.floor(Math.random() * 11)}`;
+pcValue = Math.floor(Math.random() * 11);
+console.log(pcValue);
+pcNumber.innerHTML = `<strong>PC:<strong> ${pcValue}`;
+
+const winnerDeclaration = document.getElementById('winner');
+if (userValue > pcValue) {
+    winnerDeclaration.innerHTML = `Congratulation <strong>user<strong>. You are the winner`;
+} else if (pcValue > userValue) {
+    winnerDeclaration.innerHTML = `Congratulation <strong>pc<strong>. You are the winner`;
+} else {
+    winnerDeclaration.innerHTML = `parity`;
+}
 
