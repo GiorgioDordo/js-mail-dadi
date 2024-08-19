568197e638a2ec0f028cb1fc3f561b88509f162d
GiorgioDordo <djole6789@gmail.com>
2024-08-19 17:29:26 +0200

    first commit

---
+++ dadi/script.js
@@ -0,0 +1,8 @@
+console.log('ok');
+
+const userNumber = document.getElementById('user');
+userNumber.innerHTML = `<strong>PC:<strong> ${Math.floor(Math.random() * 11)}`;
+
+const pcNumber = document.getElementById('pc');
+pcNumber.innerHTML = `<strong>PC:<strong> ${Math.floor(Math.random() * 11)}`;
+
