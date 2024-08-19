e086973807e5a1873239a058c89178ac27cdf327
GiorgioDordo <djole6789@gmail.com>
2024-08-19 18:04:53 +0200

    fixed some names

--- dadi/script.js
+++ dadi/script.js
@@ -3,7 +3,7 @@ console.log('ok');
 const userNumber = document.getElementById('user');
 userValue = Math.floor(Math.random() * 11);
 console.log(userValue);
-userNumber.innerHTML = `<strong>PC:<strong> ${userValue}`;
+userNumber.innerHTML = `<strong>USER:<strong> ${userValue}`;
 
 const pcNumber = document.getElementById('pc');
 pcValue = Math.floor(Math.random() * 11);
@@ -12,9 +12,9 @@ pcNumber.innerHTML = `<strong>PC:<strong> ${pcValue}`;
 
 const winnerDeclaration = document.getElementById('winner');
 if (userValue > pcValue) {
-    winnerDeclaration.innerHTML = `Congratulation <strong>user<strong>. You are the winner`;
+    winnerDeclaration.innerHTML = `Congratulation <strong>user</strong>, you are the winner`;
 } else if (pcValue > userValue) {
-    winnerDeclaration.innerHTML = `Congratulation <strong>pc<strong>. You are the winner`;
+    winnerDeclaration.innerHTML = `Congratulation <strong>pc</strong>, you are the winner`;
 } else {
     winnerDeclaration.innerHTML = `parity`;
 }
