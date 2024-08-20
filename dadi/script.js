d94bdf534b94e2b37ed3daf12f4973be76fdead8
GiorgioDordo <djole6789@gmail.com>
2024-08-20 15:24:31 +0200

    fixed dice exercise

--- dadi/script.js
+++ dadi/script.js
@@ -1,19 +1,19 @@
 console.log('ok');
 
 const userNumber = document.getElementById('user');
-userValue = Math.floor(Math.random() * 11);
+userValue = Math.floor(Math.random() * 7);
 console.log(userValue);
 userNumber.innerHTML = `<strong>USER:<strong> ${userValue}`;
 
-const pcNumber = document.getElementById('pc');
-pcValue = Math.floor(Math.random() * 11);
-console.log(pcValue);
-pcNumber.innerHTML = `<strong>PC:<strong> ${pcValue}`;
+const cpuNumber = document.getElementById('pc');
+cpuValue = Math.floor(Math.random() * 7);
+console.log(cpuValue);
+cpuNumber.innerHTML = `<strong>PC:<strong> ${cpuValue}`;
 
 const winnerDeclaration = document.getElementById('winner');
-if (userValue > pcValue) {
+if (userValue > cpuValue) {
     winnerDeclaration.innerHTML = `Congratulation <strong>user</strong>, you are the winner`;
-} else if (pcValue > userValue) {
+} else if (cpuValue > userValue) {
     winnerDeclaration.innerHTML = `Congratulation <strong>pc</strong>, you are the winner`;
 } else {
     winnerDeclaration.innerHTML = `parity`;
